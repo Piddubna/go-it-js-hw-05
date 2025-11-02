774c7735ad6faadc3e2adac6f8cc84d8e1a17913
Piddubna.Svitlana <poddubnaya1969@gmail.com>
2025-11-02 20:34:04 +0200

    first commit

---
+++ js/task-1.js
@@ -0,0 +1,16 @@
+'use strict';
+// function getExtremeScores(scores) {
+// let best = Math.max(...scores);
+// let worst = Math.min(...scores);
+// let object = {
+//     best, 
+//     worst,
+// }
+// return object;
+// getExtremeScores();
+// };
+// console.log(getExtremeScores([90, 85, 100, 70, 95])); // Output: "best: 100, worst: 70"
+// console.log(getExtremeScores([89, 64, 42, 17, 93, 51, 26]));
+// console.log(getExtremeScores([19, 7, 4, 17, 81, 24]));
+
+
